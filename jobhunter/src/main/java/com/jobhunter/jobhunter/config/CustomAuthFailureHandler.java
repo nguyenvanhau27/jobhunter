947407b221a56e55dc8984e52e0b2f2b7900a7d5
nguyenvanhau27 <vanhau27062001@gmail.com>
2026-04-20 00:27:01 +0700

    fix application.properties

--- jobhunter/src/main/java/com/jobhunter/jobhunter/config/CustomAuthFailureHandler.java
+++ jobhunter/src/main/java/com/jobhunter/jobhunter/config/CustomAuthFailureHandler.java
@@ -3,7 +3,6 @@ package com.jobhunter.jobhunter.config;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
