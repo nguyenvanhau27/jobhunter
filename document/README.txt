bbadb433c9863874ccfb4f30588661c19cd268b0
nguyenvanhau27 <vanhau27062001@gmail.com>
2026-04-21 21:11:47 +0700

    push doc final

--- document/README.txt
+++ document/README.txt
@@ -3,3 +3,6 @@ Task 1: Đã hoàn thành - Chờ Review
 Task 2: Đã hoàn thành - Chờ Review
 Task 3: Đã hoàn thành - Chờ Review
 Task 4: Đã hoàn thành - Chờ Review
+Task 5: Đã hoàn thành - Chờ Review
+Task 6: Đã hoàn thành - Chờ Review
+Task 7: Đã hoàn thành - Chờ Review
--- jobhunter/src/main/java/com/jobhunter/jobhunter/entity/AppEnums.java
+++ jobhunter/src/main/java/com/jobhunter/jobhunter/entity/AppEnums.java
@@ -44,23 +44,4 @@ public class AppEnums {
     }
 
 }
-//    }
-//    // Create file JobType.java
-//    public enum JobType {FULLTIME, PART_TIME, INTERN, REMOTE}
-//
-//    // Create file ExperienceLevel.java
-//    public enum ExperienceLevel {JUNIOR, MID, SENIOR}
-//
-//    // Create file ApplicationStatus.java
-//    public enum ApplicationStatus {PENDING, APPROVED, REJECTED}
-//
-//    // Create file UserStatus.java
-//    public enum UserStatus {ACTIVE, INACTIVE}
-//
-//    // Create file JobStatus.java
-//    public enum JobStatus {OPEN, CLOSED}
-//
-//    public enum SkillLevel {
-//        BEGINNER, INTERMEDIATE, ADVANCED
-//    }
-//}
+
